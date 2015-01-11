b9da0e935074e71207b6cf225896bd52121882b3
dhacker29 <dhackerdvm@gmail.com>
2015-01-11 14:46:17 -0500

    Shamu: Include radio/bootloader in zip
    Users often never update once they unlock the nexus device, leading
    to out of date firmware on the device, so lets just include it.
    
    Package: shamu-lrx22c-factory-ff173fc6
    
    Change-Id: I31ec598a81d97b73b6494b103faa1c8a38c0bdb3

---
+++ AndroidBoard.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+#----------------------------------------------------------------------
+# Radio image
+#----------------------------------------------------------------------
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+    $(call add-radio-file,radio/$(f)))
