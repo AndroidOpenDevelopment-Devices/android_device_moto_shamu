63f2202f965ae22c238755d22b5c643edc96483e
papi92 <papi92@droid-hive.com>
2015-03-10 08:45:09 -0400

    Revert "shamu: camera: Use direct project pathmap"
    This reverts commit bf1038daae2797c43f1d696f88b04180a7cd78aa.

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -41,7 +41,7 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/stack/common \
         frameworks/native/include/media/hardware \
         frameworks/native/include/media/openmax \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
+        hardware/qcom/media/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../mm-image-codec/qexif \
         $(LOCAL_PATH)/../mm-image-codec/qomx_core \
@@ -56,9 +56,9 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
 LOCAL_C_INCLUDES += \
-        $(call project-path-for,qcom-display)/libgralloc
+        hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libgralloc
 LOCAL_C_INCLUDES += \
-        $(call project-path-for,qcom-display)/libqdutils
+        hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libqdutils
 
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
